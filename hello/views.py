9ac08e9683e79ffcd2928ac27113bd9db0a18d10
Patrick Thill <kevin.patrick.thill@gmail.com>
2015-01-28 10:48:44 -0800

    Heroku teapot demo

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
-
+    r = requests.get('http://httpbin.org/status/418')
+    print r.text
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 def db(request):
 
@@ -16,4 +18,3 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-
